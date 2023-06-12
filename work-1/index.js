b4f65a4c6c3fc4fbfbeaf4a3d10c728621ca4150
Viktoriia Tomchuk <kisa6485@gmail.com>
2023-06-12 13:16:57 +0300

    Work 1 done.

---
+++ work-1/index.js
@@ -0,0 +1,8 @@
+//Треба реалізувати просту програму з наступними умовами:
+//
+// За допомогою prompt запитати ім'я користувача.
+
+const name = prompt("Write down your name here.");
+
+// За допомогою alert вивести "Hello, John! How are you?" , де John це те, що ввів користувач
+alert("Hello, " + name + "! How are you?")
