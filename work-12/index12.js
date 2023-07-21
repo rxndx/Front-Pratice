71ca4d923dd6ce734c2b31b5fd4c275ebae11116
Viktoriia Tomchuk <kisa6485@gmail.com>
2023-07-21 11:54:50 +0300

    Work-12 done

---
+++ work-12/index12.js
@@ -0,0 +1,17 @@
+'use strict'
+
+document.addEventListener("DOMContentLoaded", () => {
+    const todoList = document.querySelector("#todoList");
+    const msgInput = document.querySelector("#msgInput");
+    const msgButton = document.querySelector("#msgButton");
+
+    msgButton.addEventListener("click", () => {
+        const inputValue = msgInput.value.trim();
+        if (inputValue !== "") {
+            const listItem = document.createElement("li");
+            listItem.textContent = inputValue;
+            todoList.appendChild(listItem);
+            msgInput.value = "";
+        }
+    });
+});
